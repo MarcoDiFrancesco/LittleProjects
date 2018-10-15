f4803f8ca0a4fc299b5ec99cab275f1f16e086a5
Marco <marcodifran@gmail.com>
2018-10-15 21:05:26 +0200

    reverse array added

---
+++ C/2018-10-15 reverse an array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+Data una sequenza di caratteri in
+ingresso, riscriverla in ordine inverso.
+Si assuma che la fine della sequenza
+venga indicata da un carattere ( es. %).
+*/
+
+int main() {
+  int array[5] = {1,4,5,2,3};
+  int counter;
+  int reverseArray[5];
+  int arraySize = sizeof(array)/sizeof(array[0]);
+
+  int reverseCount = arraySize - 1; // minus one because the array 
+    // is long 5 and we want the counter starting from array[4]
+
+  for(counter = 0; counter < arraySize; counter++) {
+    reverseArray[counter] = array[reverseCount];
+    reverseCount--;
+    printf("%d ", reverseArray[counter]);
+  }
+}
