e6884402e16c88af0345117e8bc768d635d15281
AlessandroDalpiaz <44235430+AlessandroDalpiaz@users.noreply.github.com>
2018-10-17 17:15:20 +0200

    2018-10-17
    Update Grafico

--- C/2018-09-30 getchar and putchar.c
+++ C/2018-09-30 getchar and putchar.c
@@ -3,13 +3,18 @@
 
 int main(){
   int c=getchar(); // take the first input character
+	printf("numero inserito:");
 	putchar(c); // the variable must be int
+	printf("\n");
 	int counter=0;
-	while((c=getchar()) != EOF){ // read untill the end of the string
-			// EOF = end of file, to call it ctrl + c
+	while((c=getchar()) != EOF)
+	{ 	// read untill the end of the string
+		// EOF = end of file, to call it ctrl + c
 		counter++;
+		printf("numero inserito:");
 		putchar(c);
+		printf("\n");
 	}
-	printf("Number of character: %d", counter);
+	printf("\nNumber of character: %d", counter);
   return 0;
 }
