adfec7a9ed93a2354e88735185d020e54753e19d
Marco <marcodifran@gmail.com>
2018-10-26 13:51:03 +0200

    Added powershell code to compile directly in visual studio

--- C/2018-10-25 array lecture.c
+++ C/2018-10-25 array lecture.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 int main(){
-  int grades[5];
+  int grades[3] = {3,4,5};
   printf("Grades");
+
+  // clear;gcc '.\c\2018-10-25 array lecture.c';if($LASTEXITCODE -eq 0){./a.exe}else{Write-Host error compiling}
 }
