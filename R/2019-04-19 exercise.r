8e97ff12f123d232cd07b44dd7043591d89df654
Marco <marcodifran@gmail.com>
2019-04-20 20:09:49 +0200

    20th april exercise uploaded

---
+++ R/2019-04-19 exercise.r
@@ -0,0 +1,12 @@
+mean <- 29.5 # mu
+variance <- 41.6025 # sigma^2
+sd <- sqrt(variance) # sigma
+
+n1 <- (33.757-mean)/variance
+print(n1,digits=22)
+
+n2 <- pnorm(33.757, mean, sd)
+print(n2,digits=22)
+
+n3 <- 1 - pnorm(39.949, mean, sd)
+print(n3,digits=22)
