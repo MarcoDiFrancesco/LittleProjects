2f930a714a56307226b1ada8f33a946d2560d9a4
Marco <marcodifran@gmail.com>
2019-05-02 12:01:04 +0200

    Exercise 29th april updated

--- R/2019-04-29 exercise.r
+++ R/2019-04-29 exercise.r
@@ -10,5 +10,10 @@ excusionProb <- pnorm(4.6998451, mean, sd) + 1 - pnorm(5.9903836, mean, sd)
 n2 <- 1/excusionProb
 print(n2,digits=22)
 
-n3 <- pnorm(4.6998451, mean, sd)
+n3 <- (4.6998451-5.33)/0.47
 print(n3,digits=22)
+
+n4 <- pnorm(4.6998451, mean, sd)
+n5 <- pnorm(5.9903836, mean, sd)
+print(n4,digits=22)
+print(n5,digits=22)
---
+++ R/2019-05-01 exercise.r
@@ -0,0 +1,2 @@
+n4 <- function(z) {(1/sqrt(2*pi))*exp((-1/2)*z^2)} # normal distribution
+print(n4(0))
