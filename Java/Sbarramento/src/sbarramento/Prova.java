93ef678cf91e24bd6ebb49c63a681cdee4848fb0
Marco Di Francesco <marcodifran@gmail.com>
2019-06-30 20:50:29 +0200

    Last testing sbarramento project update, tomorrow EXAM

--- Java/Sbarramento/src/sbarramento/Prova.java
+++ Java/Sbarramento/src/sbarramento/Prova.java
@@ -1,31 +1,20 @@
 package sbarramento;
 
-public class B {
-    static int x = 2;
-    B() { x++; }
-    B(int y) {x=x+y;}
-    public void finalize(){x--;}
-    public static void main(String[] args) {
-        A a2 = new A();
-        System.out.print(a2);
-        A a1 = new A(2);
-        System.out.print(a1);
+class A {
+    A(int x) {System.out.println("X");}
+    A() {System.out.println("Z");}
+    public void finalize() {System.out.println("Y");}
+}
+class B extends A {
+    B(int x) {System.out.println("A");}
+    B() {System.out.println("C");}
+    public void finalize() {System.out.println("B");}
+}
+public class Prova {
+    public static void main(String args[]) {
+        A a=new B(3);
+        a = null;
+        System.gc();
+        System.runFinalization();
     }
-    public String toString(){return ""+x;}
 }
-class A extends B {
-    int x=4;
-    A() { x++; }
-    A(int x) {super(x); x++;}
-    public String toString(){return super.toString()+x;}
-}
-/*
-A
-x=super(2)
-x++
-super.tostring + x
-
-B
-y=2
-x=4
-*/
