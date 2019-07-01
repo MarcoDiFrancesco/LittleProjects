116b7b0745e3b2b36d4768bc43f726b1a6912f91
Marco Di Francesco <marcodifran@gmail.com>
2019-07-01 16:20:27 +0200

    Rename all java with dates for consistency with the old files

---
+++ Java/2019-06-28Sbarramento/src/sbarramento/Test.java
@@ -0,0 +1,40 @@
+package sbarramento;
+
+public class Test {
+
+  public static void main(String[] args) {
+    A aa = new A();
+    A ab = new B();
+    B bb = new B();
+    
+    aa.g(aa); // A 
+    aa.g(ab); // A
+    aa.g(bb); // A
+    
+    ab.g(aa); // B 1
+    ab.g(ab); // B 1
+    ab.g(bb); // B 1  
+    
+    bb.g(aa); // B 1
+    bb.g(ab); // B 1
+    bb.g(bb); // B 2  
+  }
+}
+
+class A {
+
+  void g(A aa) {
+    System.out.println("A");
+  }
+}
+
+class B extends A {
+
+  void g(A aa) {
+    System.out.println("B 1");
+  }
+
+  void g(B bb) {
+    System.out.println("B 2");
+  }
+}
