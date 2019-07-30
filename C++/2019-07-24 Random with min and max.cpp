dae1289660215d59f0d5cec2d8b35475855995ab
Marco Di Francesco <marcodifran@gmail.com>
2019-07-30 11:43:35 +0200

    Random values in c++ add

--- C++/2019-07-24 Random with min and max.cpp
+++ C++/2019-07-24 Random with min and max.cpp
@@ -4,12 +4,34 @@
 
 using namespace std;
 
+int randInt(int,int);
+int calculateAverage(int[]);
+
 int main() {
-	randValue = randBool(100.0,150.0);
+	int numbers[1000];
+	
+	int average = calculateAverage(numbers);
+
+	cout << average;
 	return 0;
 }
 
-float randBool(float min, float max) {
-	float result = min + rand()%max;
+int randInt(int min, int max) {
+	srand(time(NULL));
+	int result = min + rand()%(max-min);
 	return result;
+}
+
+int calculateAverage(int numbers[1000]) {
+	int sum = 0;
+
+	// fill array with random numbers
+	for(int i = 0; i < 1000; i++) {
+		int randomNumber = randInt(100,200);
+		cout << randomNumber << endl;
+		numbers[i] = randomNumber;
+		sum += randomNumber;
+	}
+	int average = sum / 1000;
+	return average;
 }
