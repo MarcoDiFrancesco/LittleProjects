d589e59feb0df03504624cc2144bcabc25bf35c3
Marco Di Francesco <marcodifran@gmail.com>
2019-10-14 16:11:09 +0200

    Add bank exercise and update matrix

--- C++/2019-10-08 Submatrix of numbers/src/main.cpp
+++ C++/2019-10-08 Submatrix of numbers/src/main.cpp
@@ -1,6 +1,48 @@
+#include <iostream>
+#include <stdlib.h>
 #include <fstream>
+using namespace std;
 
 int main() {
   ifstream in;
-  
+  in.open("input.txt");
+  int nRows, nCols;
+  in >> nRows;
+  in >> nCols;
+  int matrix[nRows][nCols];
+
+  // Input matrix
+  for(int i = 0; i < nRows; i++) {
+    for(int w = 0; w < nCols; w++) {
+      in >> matrix[i][w];
+    }
+  }
+
+  int sum = 0, sumMax = 0;
+  for(int i = 0; i < nRows; i++) {
+    for(int w = 0; w < nCols; w++) {
+      for(int y = i; y < nRows; y++) {
+        for(int z = w; z < nCols; z++) {
+          sum += matrix[y][z];
+          if(sum > sumMax) {
+            sumMax = sum;
+          }
+        }
+      }
+      sum = 0;
+    }
+  }
+
+  // Print
+  for(int i = 0; i < nRows; i++) {
+    for(int w = 0; w < nCols; w++) {
+      cout << matrix[i][w] << "\t";
+    }
+    cout << endl;
+  }
+  cout << endl;
+  cout << sumMax;
+  ofstream out;
+  out.open("output.txt");
+  out << sumMax;
 }
