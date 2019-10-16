3c6b1166a14390018eaed3bea789320a297bd9bd
Marco Di Francesco <marcodifran@gmail.com>
2019-10-16 09:54:08 +0200

    Fix merge sort now working. need to use less memory

--- C++/2019-10-15 Merge sort algorithm/src/main.cpp
+++ C++/2019-10-15 Merge sort algorithm/src/main.cpp
@@ -3,8 +3,9 @@
 #include <fstream>
 using namespace std;
 
-void mergeSort(int *a, int left, int right);
-void merge(int *a, int left, int middle, int right);
+void mergeSort(int *, int, int);
+void merge(int *, int, int, int);
+void print(int *, int);
 
 int main() {
   ifstream in;
@@ -15,22 +16,20 @@ int main() {
   for(int i = 0; i < n; i++) {
     in >> a[i];
   }
+  print(a, n);
+  mergeSort(a, 0, n-1);
+  // print(a, n);
+  ofstream out;
+  out.open("output.txt");
   for(int i = 0; i < n; i++) {
-    cout << a[i] << "\t";
+    out << a[i] << " ";
   }
-  cout << endl;
-
-  mergeSort(a, 0, n);
-
-  for(int i = 0; i < n; i++) {
-    cout << a[i] << "\t";
-  }
-  cout << endl;
 }
 
 void mergeSort(int *a, int left, int right) {
   if(left < right) {
     int middle = (right+left)/2;
+    // cout << left << " " << right << "\t";
     mergeSort(a, left, middle);
     mergeSort(a, middle+1, right);
     merge(a, left, middle, right);
@@ -38,32 +37,59 @@ void mergeSort(int *a, int left, int right) {
 }
 
 void merge(int *a, int left, int middle, int right) {
-  int temp[right-left+1];
-  int l = left;
-  int m = middle+1;
-  int k = 0;
-  while(l < middle && m <= right) {
-    if(a[l] <= a[m]) {
-      temp[k] = a[l];
-      k += 1;
-      l += 1;
-    } else {
-      temp[k] = a[m];
-      k += 1;
-      m += 1;
+  int nElem = right - left + 1;
+  if(nElem == 0) { // Impossible case
+    cout << "Impossible case: error" << endl;
+    exit(1);
+  } else if(nElem == 1) { // Ordered element
+    return;
+  } else if(nElem == 2) { // 2 elements
+    if(a[left] > a[right]) {
+      int temp = a[right];
+      a[right] = a[left];
+      a[left] = temp;
+    }
+    // cout << endl << a[right] << a[left] << endl;
+  } else { // More than 2 elements
+    int temp[nElem];
+    int n1 = left;
+    int n2 = middle + 1;
+    cout << left << " " << right << " " << "\t";
+    for(int i = left; i < middle + 1; i++) {
+      cout << a[i] << " ";
+    }
+    cout << "\t\t";
+    for(int i = middle + 1; i < right+1; i++) {
+      cout << a[i] << " ";
+    }
+    cout << endl;
+
+    for(int i = 0; i < nElem; i++) {
+      if(a[n1] < a[n2] && n1 <= middle) {
+        temp[i] = a[n1];
+        n1++;
+      } else if (n2 <= right){
+        temp[i] = a[n2];
+        n2++;
+      } else {
+        temp[i] = a[n1];
+        n1++;
+      }
+    }
+    print(temp, nElem);
+
+    int k = 0;
+    for(int i = left; i < right+1; i++) {
+      a[i] = temp[k];
+      k++;
     }
   }
-  while(l <= middle) {
-    temp[k] = a[l];
-    k += 1;
-    l += 1;
+}
+
+// Prints array
+void print(int *a, int n) {
+  for(int i = 0; i < n; i++) {
+    cout << a[i] << "\t";
   }
-  while(m <= right) {
-    temp[k] = a[m];
-    k += 1;
-    m +=1;
-  }
-  for(l = left; l <= right; l += 1) {
-    a[l] = temp[l-left];
-  }
+  cout << endl;
 }
