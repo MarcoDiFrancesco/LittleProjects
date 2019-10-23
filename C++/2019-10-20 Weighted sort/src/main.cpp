78a3fd1ff8ce832ea0e035e1e9fb663eff6484fb
Marco Di Francesco <marcodifran@gmail.com>
2019-10-23 17:57:24 +0200

    Add input, index and get into unordered map

--- C++/2019-10-20 Weighted sort/src/main.cpp
+++ C++/2019-10-20 Weighted sort/src/main.cpp
@@ -1,43 +1,58 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
-#include <map>
+#include <unordered_map>
 using namespace std;
 
-void printArray(int *numbers, int nElem) {
+void writeValue(unordered_map<int, int> *numbers, int index, int value) {
+  // write first in second
+}
+
+void inputValues(unordered_map<int, int> *numbers) {
+  ifstream in("input.txt");
+  int nElem;
+  in >> nElem;
+  int temp;
   for(int i = 0; i < nElem; i++) {
-    cout << numbers[i] << " ";
+    in >> temp;
+    numbers->insert({i,temp});
   }
-  cout << endl;
+  in.close();
 }
 
-int getIndex(int *numbers, int value) {
-  bool check = false;
-  int i = 0;
-  int index;
-  do {
-    if(numbers[i] == value) {
-      check = true;
-      index = i;
+int getIndex(unordered_map<int,int> const *numbers, int value) {
+  auto iterator = numbers->begin();
+  for(int i = 0; i < numbers->size(); i++) {
+    if(iterator->second == value) {
+      return iterator->first;
     }
-    i++;
-  } while(check == false);
-  return index;
+    iterator++;
+  }
+  return -1;
 }
 
-int switchSingleCost(int *numbers, int nElem) {
+int getValue(unordered_map<int,int> const *numbers, int index) {
+  return numbers->at(index);
+}
+
+void print(unordered_map<int,int> const *numbers) {
+  for(int i = 0; i < numbers->size(); i++) {
+    cout << "[" << i << "]=" << getValue(numbers, i) << " ";
+  }
+}
+
+int switchSingleCost(unordered_map<int,int> const *numbers, int nElem) {
   int cost = 0;
-  for(int i = 1; i < nElem + 1; i++) {
-    // printArray(numbers, nElem);
+  for(int i = 1; i < numbers->size() + 1; i++) {
     bool found = false;
     int w = i;
     do {
-      if(i == numbers[w-1]){
+      if(i == getValue(numbers, w-1)) {
         found = true;
         if(i != w) {
           cost++;
-          int tempValue = numbers[i-1];
-          numbers[i-1] = numbers[w-1];
+          int tempValue = getValue(numbers, i-1);
+          writeValue(numbers, getIndex(i-1), getIndex(w+1));
           numbers[w-1] = tempValue;
         }
       }
@@ -49,32 +64,20 @@ int switchSingleCost(int *numbers, int nElem) {
 
 int switchValueCost(int *numbers, int nElem) {
   int cost = 0;
-  printArray(numbers, nElem);
 
   // get first non-ordered element
   // get index of it
   // exchange it with the corresponding index
   // if same match
   // call recursivly the function
-  cout << getIndex(numbers, 1) << " ";
-
+  
   return cost;
 }
 
 int main() {
-  ifstream in("input.txt");
-  int nElem;
-  in >> nElem;
-  // unordered_map<int, int> numbersMap;
-  int numbers1[nElem];
-  int numbers2[nElem];
-  int temp;
-  for(int i = 0; i < nElem; i++) {
-    in >> temp;
-    numbers1[i] = temp;
-    numbers2[i] = temp;
-  }
-  in.close();
-  int singleCost = switchSingleCost(numbers1, nElem);
-  int valueCost = switchValueCost(numbers2, nElem);
+  unordered_map<int, int> numbers;
+  inputValues(&numbers);
+  print(&numbers);
+  int singleCost = switchSingleCost(&numbers);
+//  int valueCost = switchValueCost(&numbers);
 }
