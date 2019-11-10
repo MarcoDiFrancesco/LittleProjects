992a5b2326e86fa3f173184198c5a331fc1c5324
Marco Di Francesco <marcodifran@gmail.com>
2019-11-10 17:35:14 +0100

    Refactoring Luca's exercise

--- C++/2019-11-02 Luca's exrecise.cpp
+++ C++/2019-11-02 Luca's exrecise.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <cmath>
 struct dupla {
- double a, b;
-} ;
+  double a, b;
+};
 
 using namespace std;
-int main () {
-int N = 100;
-dupla* y = new dupla[N];
-for(int i = 0; i < 10; i++) {
-  y[i].a = i;
-  cout << y[i].a << " ";
-}
-return 0;
+int main() {
+  int N = 100;
+  dupla* y = new dupla[N];
+  for(int i = 0; i < 10; i++) {
+    y[i].a = i;
+    cout << y[i].a << " ";
+  }
+  return 0;
 }
