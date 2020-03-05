7918819eec01dcba4d85f2240aaf8f0f3cbbe135
MarcoDiFrancesco <marcodifran@gmail.com>
2020-03-05 10:07:19 +0100

    Update computation test

--- Python/2020-01-26 Test computation.py
+++ Python/2020-01-26 Test computation.py
@@ -11,13 +11,10 @@ def compute():
     """
     number = 0
     start_time = time.time()
-
     for _ in range(0, 10000000):
         number = number + 1
         number = number - 1
-
     time_difference = time.time() - start_time
-
     print("--- ", time_difference, " seconds ---")
 
 
