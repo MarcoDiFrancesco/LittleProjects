d1726ea40c9cbeb3189148f0266ffa95ece3bb6a
MarcoDiFrancesco <marcodifran@gmail.com>
2020-05-13 19:41:45 +0200

    Remove unused calendar, update java servlet

--- Stramlit/2020-05-12 calendar.py
+++ Stramlit/2020-05-12 calendar.py
@@ -1,23 +1,46 @@
-import numpy as np
+import json
+import os
+import uuid
+
+import jicson
 import pandas as pd
+import requests
 import streamlit as st
 
 st.title("Calendar data")
-DATA_URL = "/home/marco/Downloads/eat-calendar.csv"
+DATA_URL = "/home/marco/projects/LittleProjects/Stramlit/test.json"
+CAL_LINK = "https://calendar.google.com/calendar/ical/gglu2jqt34qputlgj37fi6d9p8%40group.calendar.google.com/private-f6841660bb6f830eea31bb2d8d2215cf/basic.ics"
 
 
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    # data.rename(lambda x: str(x).lower(), axis="columns", inplace=True)
-    # data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+def get_data(link):
+    """
+    Download ics from Google Calendar, write it to a temp file
+    and return data in json form
+    """
+    ics = requests.get(CAL_LINK).text
+    file_path = f"/tmp/{uuid.uuid4()}-calendar.ics"
+    with open(file_path, "w") as f:
+        f.write(str(ics))
+    json = jicson.fromFile(file_path)
+    os.remove(file_path)
+    return json["VCALENDAR"][0]["VEVENT"]
+
+
+def date_edit(data):
+    """
+    Make dates in python date library format
+    """
+    for d in data:
+        for col_name in ["DTSTART", "DTEND", "DTSTAMP", "CREATED", "LAST-MODIFIED"]:
+            d[col_name] = pd.to_datetime(d[col_name])
     return data
 
 
-data_load_state = st.text("Loading data...")
-data = load_data(10000)
-data_load_state.text("")
+data = get_data(CAL_LINK)
+data = date_edit(data)
 
-print(data)
-df = pd.DataFrame(data)
+
+df = pd.DataFrame(data, columns=["DTSTART"])
+print(df)
 df
+st.line_chart(df)
--- Web/WebApplicationWithDerby/src/java/com/marcodifrancesco/DBServlet.java
+++ Web/WebApplicationWithDerby/src/java/com/marcodifrancesco/DBServlet.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,15 +24,17 @@ public class DBServlet extends HttpServlet {
     String username = "mydb";
     String password = "mydb";
     Connection conn = null;
-    
+
+    @Override
     public void init() {
         try {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
             conn = DriverManager.getConnection(dbURL, username, password);
-        } catch () {
-            
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
