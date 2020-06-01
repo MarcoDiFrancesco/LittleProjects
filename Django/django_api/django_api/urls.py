8bb09026a9f6a4ce13fadc18de416c566a882af2
MarcoDiFrancesco <marcodifran@gmail.com>
2020-06-01 18:00:01 +0200

    Add tutorial django api rest framework

--- Django/django_api/django_api/urls.py
+++ Django/django_api/django_api/urls.py
@@ -1,33 +1,19 @@
-from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.models import User
-from django.urls import path
-from rest_framework import routers, serializers, viewsets
+from django.urls import include, path
+from rest_framework import routers
 
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ["url", "username", "email", "is_staff"]
-
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
+from django_api.quickstart import views
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r"users", UserViewSet)
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    url(r"^", include(router.urls)),
-    url(r"^api-auth/", include("rest_framework.urls")),
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
