ad8c8da1c030b2e695ac0a0b2e3e2540b333eea9
MarcoDiFrancesco <marcodifran@gmail.com>
2020-09-28 19:41:12 +0200

    Update javascript notes and edit example

--- Javascript/2019-11-23 ConsoleLog and functions.js
+++ Javascript/2019-11-23 ConsoleLog and functions.js
@@ -7,3 +7,23 @@ function mergeWords(first, second) {
   return "" + first + " " + second;
 }
 console.log(mergeWords('hello', `world`));
+
+// Arrays
+let myArray = [1, 5];
+let myNestedArray = [["first", "second"], ["third", "forth"]];
+console.log("Array: " + myArray[0] + myArray[1] + myArray[2]);
+console.log("NestedArray: " + myNestedArray[0][1] + " " + myNestedArray[1][0])
+
+myArray = [2,7,4];
+console.log(myArray);
+myArray.pop();
+console.log(myArray);
+// Output: (3) [2, 7, 4]
+//         (2) [2, 7]
+
+myArray = [2,7,4];
+console.log(myArray);
+myArray.shift();
+console.log(myArray);
+// Output: (3) [2, 7, 4]
+//         (2) [2, 7]
