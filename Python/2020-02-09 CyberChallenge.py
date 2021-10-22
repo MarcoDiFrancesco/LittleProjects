831a2d9d27680987272b9590dbc4d5d09cbe513f
Francesco Bozzo <francesco.bozzo.99@gmail.com>
2021-10-22 23:46:47 +0200

    Improve code readability

--- Python/2020-02-09 CyberChallenge.py
+++ Python/2020-02-09 CyberChallenge.py
@@ -1,9 +1,10 @@
-def mistery(n):
+def mistery(n: int) -> int:
     k = 2
-    while((k*k <= n) and (n % k > 0)):
-        k = k+1
-    return (k*k > n)
+    while (k*k <= n) and (n % k > 0):
+        k += 1
+
+    return k*k > n
 
 
 for x in range(1, 100):
-    print(str(x) + " " + str(mistery(x)))
+    print(x, mistery(x))
